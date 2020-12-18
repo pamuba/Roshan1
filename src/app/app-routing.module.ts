b37ccd360fc45aaeb76e733ab9d2afffa005f2d2
pamuba <partha.bora24@gmail.com>
2020-12-19 01:08:05 +0530

    third

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,33 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { DetailsComponentComponent } from './components/details-component/details-component.component';
+import { UsersComponent } from './components/users/users.component';
 
 
 const routes: Routes = [
+  // {
+  //   path: 'details/:id',
+  //   pathMatch: 'full',
+  //   component : DetailsComponentComponent
+  // },
+  // { path: 'home', component: AppComponent },
+  // { path: '',   redirectTo: '/home', pathMatch: 'full' },
+
   {
-    path: 'details/:id',
-    pathMatch: 'full',
-    component : DetailsComponentComponent
-  },
-  { path: 'home', component: AppComponent },
-  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+    path: '',
+    children: [
+    // {
+    //   path: '',
+    //   // pathMatch: 'full',
+    //   component: UsersComponent,
+    // },
+    {
+      path: 'details/:id',
+      pathMatch: 'full',
+      component: DetailsComponentComponent
+    },
+  ]
+  }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent {
   users:Users[] = [];
   repos:Repos[] = [];
   customRepos:Repos[] = [];
+  flag:Boolean = true;
 
   constructor(private githubService:GithubService){
 
